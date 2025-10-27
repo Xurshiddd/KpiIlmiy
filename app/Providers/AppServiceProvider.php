28dbfd4d8d2e5a71cb9fdcdabb2d9b3fc5a4f18e
Xurshiddd <marifovxurshid800@gmail.com>
2025-10-27 07:36:36 +0500

    ghvhg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,7 +6,7 @@ use App\Events\UserInfoEvent;
 use App\Listeners\UserInfoListener;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -26,5 +26,7 @@ class AppServiceProvider extends ServiceProvider
             UserInfoEvent::class,
             UserInfoListener::class
         );
+        Schema::defaultStringLength(191);
+
     }
 }
