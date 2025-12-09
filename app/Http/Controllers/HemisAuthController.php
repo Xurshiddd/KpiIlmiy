d31efc0044bcb3d7f92bdb2092494680b9cdbe3a
Xurshiddd <marifovxurshid800@gmailcom>
2025-12-09 16:36:23 +0500

    D

--- app/Http/Controllers/HemisAuthController.php
+++ app/Http/Controllers/HemisAuthController.php
@@ -40,7 +40,6 @@ class HemisAuthController extends Controller
             if (!$userData) {
                 return abort(500, 'Failed to retrieve user data from Hemis');
             }
-            dd($userData);
             $employee = User::updateOrCreate(
                 ['employee_id_number' => $userData['employee_id_number']],
                 [
